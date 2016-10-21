84a7852cf53c9000ffdaf2e1e2ba1105a05e5987
Stephen Eick <prinnydude97@gmail.com>
2016-10-21 12:06:19 -0400

    Configured nginx, made the site run at just -localhost-

---
+++ config/webserver.sh
@@ -0,0 +1,17 @@
+# This file will prepare nginx to run our application on a Linux server.
+# To run this on a Mac, you'll want to research additional configuration steps.
+    # Please add the steps to configure it in here, though!
+
+# Also, make sure you run this file as sudo.
+
+if [ $(uname) != "Linux" ]; then
+    echo "You're not on Linux! Halting the configuration processs...";
+    exit;
+fi
+
+# First, let's get the site information loaded into nginx
+cp nginx/mapping_justice /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/mapping_justice /etc/nginx/sites-enabled/mapping_justice
+
+# Restart nginx
+service nginx restart;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-/*router.get('/map', function(req, res, next) {
-    res.render('map');
-});*/
+*/
+router.get('/', function(req, res, next) {
+    res.render('map', { title: 'Mapping Justice' });
+});
 
 module.exports = router;
