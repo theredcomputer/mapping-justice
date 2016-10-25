8435938b1f6b831e15e215c655f140102a226cea
KINGOFTHEWORLD <godfathergzz@gmail.com>
2016-10-25 16:04:34 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,16 +1,6 @@
 module.exports = {
 
-    'facebookAuth' : {
-        'clientID'      : 'your-secret-clientID-here', 
-        'clientSecret'  : 'your-client-secret-here', 
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-    },
-
-    'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
-    },
+ 
 
     'googleAuth' : {
         'clientID'      : 'your-secret-clientID-here',
