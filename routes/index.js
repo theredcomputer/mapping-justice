ed900e1a6ce099764c77af7121a3934e7e285979
KINGOFTHEWORLD <godfathergzz@gmail.com>
2016-10-25 21:38:37 -0400

    Updated index.js to display login and registration

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,52 @@ router.get('/', function(req, res, next) {
     res.render('map', { title: 'Mapping Justice' });
 });
 
+//Added by Kevin
+
+    router.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.jade', { message: req.flash('loginMessage') }); 
+    });
+
+ 
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.jade', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {user : req.user }); // get the user out of session and pass to template;
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+} //Added by Kevin
+
 module.exports = router;
