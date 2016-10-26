18263733028b51e87053bb68c09bc07757c7d229
KINGOFTHEWORLD <godfathergzz@gmail.com>
2016-10-25 21:24:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var map = require('./routes/map');
@@ -23,9 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', routes);
 app.use('/users', users);
 app.use('/map', map);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
