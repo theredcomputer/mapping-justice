f2fbf42f6088b9417a3a7ab4360c0c13a7053d11
hfeng49 <hfeng49@gatech.edu>
2016-11-01 15:30:33 -0400

    first integration

--- routes/map.js
+++ routes/map.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('test02.html', {title: 'The Map'});
+    res.render('map', {title: 'The Map'});
 });
 
 module.exports = router;
