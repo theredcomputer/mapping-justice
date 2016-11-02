46285a5c6d24b1096849a35913024a19989acddf
KINGOFTHEWORLD <godfathergzz@gmail.com>
2016-11-01 20:34:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,98 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var map = require('./routes/map');
-
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+//var expressSession = require('express-session');
+//app.use(expressSession({secret: 'mySecretKey'}));
+//app.use(passport.initialize());
+//app.use(passport.session());
+
+var flash = require('connect-flash');
+app.use(flash());
+
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+var flash = require('connect-flash');
+app.use(flash());
+
+var initPassport = require('./passport/init');
+initPassport(passport);
+//var flash = require('connect-flash');
+
+
+//var routes = require('./routes/index')(passport);
+//var users = require('./routes/users');
+//var map = require('./routes/map');
+
+
+//var dbConfig = require('./db');
+//var mongoose = require('mongoose');
+// Connect to DB
+//mongoose.connect(dbConfig.url);
+
+
+//var app = express();.static(path.join(__dirname, 'public')));
+
+//
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+var routes = require('./routes/index')(passport);
+var users = require('./routes/users');
+var map = require('./routes/map');
+
+var dbConfig = require('./db');
+var mongoose = require('mongoose');
+// Connect to DB
+mongoose.connect(dbConfig.url);
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/map', map);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
+// Configuring Passport
+//var passport = require('passport');
+//var expressSession = require('express-session');
+//app.use(expressSession({secret: 'mySecretKey'}));
+//app.use(passport.initialize());
+//app.use(passport.session());
+
+// Using the flash middleware provided by connect-flash to store messages in session
+// and displaying in templates
+//var flash = require('connect-flash');
+//app.use(flash());
+
+// Initialize Passport
+//var initPassport = require('./passport/init');
+//initPassport(passport);
+
+//var routes = require('./routes/index')(passport);
+//app.use('/', routes);
+
+/// catch 404 and forward to error handler
+
+//app.use(function(req, res, next) {
+  //var err = new Error('Not Found');
+  //err.status = 404;
+  //next(err);
+//});
 
 // development error handler
 // will print stacktrace
@@ -48,18 +105,5 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-
 
 module.exports = app;
