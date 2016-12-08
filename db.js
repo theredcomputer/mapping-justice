3dda3e37a9c4f8dc4a1e55d2d5a99d3a088d57f2
KINGOFTHEWORLD <godfathergzz@gmail.com>
2016-12-08 07:12:28 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 module.exports = {
-  'url' : 'mongodb://kevinhuntington:kevinhuntington@jello.modulusmongo.net:27017/ywuQus9i'
-  //'url' : 'mongodb://localhost/passport'
+  //'url' : 'mongodb://kevinhuntington:kevinhuntington@jello.modulusmongo.net:27017/ywuQus9i'
+  'url' : 'mongodb://localhost/passport'
 
 }
 
