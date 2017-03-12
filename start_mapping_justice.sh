2b0a5637738b05a6f456bd472efd852abc2ccba9
Stephen Eick <prinnydude97@gmail.com>
2017-03-12 00:50:57 -0500

    [start_mapping_justice.sh] Created a new method to start the application.\n[stop_mapping_justice.sh] Created a new method to take down the application.\n[INSTALL] Updated documentation to reflect new changes in application management.

---
+++ start_mapping_justice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This file must remain in the project root directory to spin up the
+# application properly.
+
+PATH_PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PATH_LOG_DIRECTORY=$PATH_PROJECT_ROOT/logs
+PATH_USERDATA=$PATH_PROJECT_ROOT/userdata
+PATH_MONGO_EXEC_DIR=$PATH_PROJECT_ROOT/bin
+
+DATE="$(date +%s)"
+mkdir $PATH_LOG_DIRECTORY 2> /dev/null
+
+exec 1>"$PATH_LOG_DIRECTORY"/log_db_$DATE 2>&1
+
+cd $PATH_MONGO_EXEC_DIR
+mongod --dbpath "$PATH_USERDATA" --port 27017 --smallfiles &
+sleep 5
+
+exec 1>"$PATH_LOG_DIRECTORY"/log_site_$DATE 2>&1
+
+cd $PATH_PROJECT_ROOT
+npm start &
---
+++ stop_mapping_justice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pkill node
+echo "node halted."
+
+pkill mongod
+echo "Mongo halted."
+
